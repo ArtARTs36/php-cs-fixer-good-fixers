4cccec707fea3b9cf86156704585c4569e7c2896
ArtARTs36 <temicska99@mail.ru>
2021-11-28 16:29:29 +0300

    rename test resources

--- tests/Unit/LaravelCommandNoEmptyDescriptionFixerTest.php
+++ tests/Unit/LaravelCommandNoEmptyDescriptionFixerTest.php
@@ -11,27 +11,27 @@ final class LaravelCommandNoEmptyDescriptionFixerTest extends TestCase
     {
         return [
             [
-                __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/01.php',
+                __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/01_has_empty_property.php',
                 __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/expected.php',
             ],
             [
-                __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/02.php',
+                __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/02_has_property_without_value.php',
                 __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/expected.php',
             ],
             [
-                __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/03.php',
+                __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/03_has_null_property.php',
                 __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/expected.php',
             ],
             [
-                __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/04.php',
+                __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/04_has_null_property_without_spaces.php',
                 __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/04_expected.php',
             ],
             [
-                __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/05.php',
+                __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/05_empty_body.php',
                 __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/expected.php',
             ],
             [
-                __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/06.php',
+                __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/06_has_signature_without_description.php',
                 __DIR__ . '/../resources/laravel_command_no_empty_description/test_fix/06_expected.php',
             ],
             [
